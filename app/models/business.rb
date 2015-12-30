95dd2ce8e122311c554bf363850965df6bcedcfe
Prelang Builder <builder@prelang.com>
2015-12-30 07:01:32 +0000

    Defined validation for attribute 'zip' on model 'Business'

--- app/models/business.rb
+++ app/models/business.rb
@@ -3,4 +3,5 @@ class Business < ActiveRecord::Base
   belongs_to :order
   has_many :locations
   validates_formatting_of :phone_number, using: :us_phone
+  validates_formatting_of :zip, using: :us_zip
 end
